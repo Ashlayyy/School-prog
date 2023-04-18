37331044e11cb716fde943a7a28d4102d92c5461
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-04-18 11:07:59 +0200

    Add UrlScrapper, Api, PrettyPlatform, Filter and App Class in Eindopdracht

---
+++ 04-Backlog/js/main.js
@@ -0,0 +1,83 @@
+class Api {
+    data = undefined;
+
+    getData = async () => {
+        await fetch('./data/games.json').then(async res => {
+            return await res.json();
+        }).then(data => {
+            this.data = data.games;
+        })
+        return;
+    }
+}
+
+class UrlScraper {
+    currentUrl;
+    platform;
+    Pretty;
+
+    constructor() {
+        this.currentUrl = window.location.href;
+    }
+
+    getDataFromUrl = () => {
+        this.platform = this.currentUrl.split('platform=')[1];
+        this.Pretty = new PrettyPlatform(this.platform);
+        this.platform = this.Pretty.platform;
+    }
+}
+
+class PrettyPlatform {
+    platform;
+    constructor (platform) {
+        this.platform = platform;
+        this.toUperCase();
+        this.removeWhiteSpace();
+    }
+
+    toUperCase = () => {
+        this.platform = this.platform.toUpperCase();
+    }
+
+    removeWhiteSpace = () => {
+        this.platform = this.platform.replace(' ', '');
+        this.platform = this.platform.replace('%20', '')
+    }
+}
+
+class Filter {
+    results = [];
+
+    filter = (platform, data) => {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].platform == platform) {
+                this.results.push(data[i]);
+            }
+        }
+        this.randomFromResult(this.results);
+    }
+
+    randomFromResult = () => {
+        let randomNumber = Math.floor(Math.random() * this.results.length);
+        console.log(this.results[randomNumber]);
+    }
+}
+
+class App {
+    Api = undefined;
+    filter = undefined;
+    urlScraper = undefined;
+
+    constructor() {
+        this.Api = new Api();
+        this.filter = new Filter();
+        this.urlScraper = new UrlScraper();
+
+        this.urlScraper.getDataFromUrl();
+        this.Api.getData().then(() => {
+            this.filter.filter(this.urlScraper.platform, this.Api.data)
+        });
+    }
+}
+
+const app = new App();
