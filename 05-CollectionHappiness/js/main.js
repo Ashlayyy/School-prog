c66dfc3dfeb7951ff50f40c30dc27717d4e2529b
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-04-20 13:26:16 +0200

    Setup Main.js with classes

---
+++ 05-CollectionHappiness/js/main.js
@@ -0,0 +1,51 @@
+class Header {
+    constructor() {
+
+    }
+}
+
+class Footer {
+    constructor() {
+
+    }
+}
+
+class LeftPanel {
+    constructor() {
+
+    }
+}
+
+class DetailCard {
+    constructor() {
+
+    }
+}
+
+class RightPanel {
+    DetailCard = undefined;
+    
+    constructor() {
+
+    }
+}
+
+class GetData {
+    url = undefined;
+
+    constructor () {
+
+    }
+}
+
+class App {
+    Header = undefined;
+    Footer = undefined;
+    LeftPanel = undefined;
+    RightPanel = undefined;
+    GetData = undefined;
+
+    constructor() {
+
+    }
+}
